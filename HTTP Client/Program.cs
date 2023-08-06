c8c0a27a47e81ce07f8ed54d3011db6781517a96
semsi <semsi@KENN>
2023-08-06 06:00:11 +0400

    Proje dosyası ekle.

---
+++ HTTP Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Net;
+using System.Text;
+using System;
+
+using(var client = new HttpClient())
+{
+    string link = "http://ocalhost:27001/";
+    string mesg = "POST";
+
+    var content = new StringContent(mesg, Encoding.UTF8, "text/plain");
+    var response = await client.PostAsync(link, content);
+
+
+    if (response.IsSuccessStatusCode)
+    {
+        string responseContent = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(responseContent);
+    }
+    else
+    {
+        Console.WriteLine("error");
+    }
+
+}
+
+
+
+class User
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+}
+
---
+++ Network Practic http/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using static System.Net.WebRequestMethods;
+
+
+var listnr = new HttpListener();
+
+listnr.Prefixes.Add(@"http://localhost:27001/");
+
+listnr.Start();
+
+Console.WriteLine("Listening...");
+
+while (true)
+{
+    var context = listnr.GetContext();
+    var rq = context.Request;
+    var rp = context.Response;
+
+
+    if (rq.HttpMethod == "POST")
+    {
+        StreamWriter streamWriter = new StreamWriter(rp.OutputStream);
+
+        streamWriter.WriteLine($"<h1>salam {rq.QueryString["Name"]}</h1>");
+        streamWriter.Close();
+    }
+    else if (rq.HttpMethod == "PUT")
+    {
+        StreamWriter streamWriter = new StreamWriter(rp.OutputStream);
+    }
+}
+
+
+
+
+List<User> users = new List<User>
+{
+    new User { Id = 1, Name = "Nerbala", Surname = "Xaladennikov" },
+    new User { Id = 2, Name = "name1", Surname = "surname1" }
+};
+
+class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+}
